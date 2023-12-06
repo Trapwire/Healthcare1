14b3d056584b0b615840b3f9cd9ad1271e7f7333
Trapwire <ujjwalkhushhai@gmail.com>
2023-12-06 13:04:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -459,6 +459,11 @@ def chat_msg():
 
         return jsonify({'status': 'OK', 'answer': response})
 
+if __name__ == "__main__":
+
+    db.create_all()
+    app.run(debug=False, host='0.0.0.0')
+
 
 
 
